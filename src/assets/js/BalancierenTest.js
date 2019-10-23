8906e8528408d7dd01bf8c009b18b5bf3f65ea16
Hackerberger <hechenberger.l00@htl-ottakring.ac.at>
2019-10-23 15:49:20 +0200

    Balancieren Berechnung eingefügt

---
+++ src/assets/js/BalancierenTest.js
@@ -0,0 +1,22 @@
+/*
+Funktion bekommt Objekt übergeben:
+{
+    "6cm":
+
+}
+*/
+
+var testobject = {
+    "6cm": [5,2],
+    "4cm": [4,3],
+    "3cm": [6,5]
+}
+
+function Balance(bData) {
+    let bpoints;
+
+
+
+
+    return bpoints;
+}
