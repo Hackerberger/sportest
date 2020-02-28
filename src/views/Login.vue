b0fa9abda030e9788915872d149df0e70907c168
burningman97 <50482858+burningman97@users.noreply.github.com>
2020-02-28 11:33:52 +0100

    Login (Done ... at least for now!)

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,57 +2,37 @@
   <v-container fluid fill-height>
     <v-layout align-center column fill-height>
       <v-flex align-center justify-center column fill-height>
-        <g-signin-button
-          :params="googleSignInParams"
-          @success="onSignInSuccess"
-          @error="onSignInError"
-        >
-          <v-btn id="googleButton" to="home_l" color="#161616" large>
-            <img id="img-button" src="../../public/img/Google/google_logo.png">
-            <span>Log In mit Google</span>
-          </v-btn>
-        </g-signin-button>
+        <button v-google-signin-button="clientId" class="google-signin-button">
+           <img id="img-button" src="../../public/img/Google/google_logo.png">
+           <v-divider></v-divider>
+          <span>Google-Login</span>  
+            </button>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
  
 <script>
-var deferredPrompt;
 
+
+import GoogleSignInButton from 'vue-google-signin-button-directive'
 export default {
-  data() {
-    return {
-      googleSignInParams: {
-        client_id:
-          "419461260696-531a21v1lv63e72ckd0rts3j72bl2tf5.apps.googleusercontent.com"
-      },
-      installPossible: false,
-    };
+  directives: {
+    GoogleSignInButton
   },
-  created() {
-
-    //Save Install Prompt
-    window.addEventListener("beforeinstallprompt", e => {
-      // Prevent Chrome 67 and earlier from automatically showing the prompt
-      e.preventDefault();
-      // Stash the event so it can be triggered later.
-      deferredPrompt = e;
-
-      this.installPossible = true;
-    });
-  },
+  data: () => ({
+    clientId: '419461260696-531a21v1lv63e72ckd0rts3j72bl2tf5.apps.googleusercontent.com'
+  }),
   methods: {
-    async onSignInSuccess(googleUser) {
-      //googleUser für alle Components verfügbar machen
-      // this.$parent.globalData = googleUser;
+    OnGoogleAuthSuccess (googleUser) {
+      this.$parent.globalData = googleUser;
 
       // `googleUser` is the GoogleUser object that represents the just-signed-in user.
       // See https://developers.google.com/identity/sign-in/web/reference#users
       // const profile = googleUser.getBasicProfile();
 
-      //this.$emit("googleUserData", profile);
-      //this.googleData = googleUser.getBasicProfile();
+      // this.$emit("googleUserData", profile);
+      // this.googleData = googleUser.getBasicProfile();
       // console.log("ID: " + profile.getId());
       // console.log("Full Name: " + profile.getName());
       // console.log("Given Name: " + profile.getGivenName());
@@ -60,8 +40,7 @@ export default {
       // console.log("Image URL: " + profile.getImageUrl());
       // console.log("Email: " + profile.getEmail());
 
-      // if (this.installPossible) this.install();
-
+      if (this.installPossible) this.install();
       this.$router.push({ name: "home" });
     },
     install() {
@@ -87,33 +66,152 @@ export default {
         }
       }
     },
-    onSignInError(error) {
-      console.log("OH NO", error);
+    OnGoogleAuthFail (error) {
+      console.log(`Anmeldung fehlgeschlagen: ${error}`)
     }
   }
-};
+}
+
 </script> 
  
 <style>
-body {
-  background-color: white;
-}
-.g-signin-button {
+.google-signin-button {
   display: inline-block;
   padding: 4px 8px;
   border-radius: 3px;
-  background-color: none;
+  background-color: white;
   margin-top: 55%;
-  color: white;
+  font-family: Roboto;
+  color: black;
+}
+body {
+  background-color: white;
 }
 #img-button {
-  position: relative;
-  /* Adjust these values accordingly */
-  top: 4px;
-  left: -8px;
+  margin-top: 5px;
+
 }
 #googleButton {
   height: 50px;
   color: darkslategray;
 }
+
+#g-log-text {
+  margin-bottom: 50%; 
+}
+
+
+
+
+
+
+
+
+
+
+    /* <!-- <g-signin-button
+          :params="googleSignInParams"
+          @success="onSignInSuccess"
+          @error="onSignInError"
+        >
+        
+          <v-btn id="googleButton" to="home_l" color="#161616" large>
+            <img id="img-button" src="../../public/img/Google/google_logo.png">
+            <span>Log In mit Google</span>
+          </v-btn>
+        </g-signin-button> --> */
+
+/* export default {
+//   data() {
+//     return {
+//       googleSignInParams: {
+//         client_id:
+//           "419461260696-531a21v1lv63e72ckd0rts3j72bl2tf5.apps.googleusercontent.com"
+//       },
+//       installPossible: false,
+//     };
+//   },
+//   created() {
+
+//     //Save Install Prompt
+//     window.addEventListener("beforeinstallprompt", e => {
+//       // Prevent Chrome 67 and earlier from automatically showing the prompt
+//       e.preventDefault();
+//       // Stash the event so it can be triggered later.
+//       deferredPrompt = e;
+
+//       this.installPossible = true;
+//     });
+//   },
+//   methods: {
+//     async onSignInSuccess(googleUser) {
+//       //googleUser für alle Components verfügbar machen
+//       // this.$parent.globalData = googleUser;
+
+//       // `googleUser` is the GoogleUser object that represents the just-signed-in user.
+//       // See https://developers.google.com/identity/sign-in/web/reference#users
+//       // const profile = googleUser.getBasicProfile();
+
+//       //this.$emit("googleUserData", profile);
+//       //this.googleData = googleUser.getBasicProfile();
+//       // console.log("ID: " + profile.getId());
+//       // console.log("Full Name: " + profile.getName());
+//       // console.log("Given Name: " + profile.getGivenName());
+//       // console.log("Family Name: " + profile.getFamilyName());
+//       // console.log("Image URL: " + profile.getImageUrl());
+//       // console.log("Email: " + profile.getEmail());
+
+//       // if (this.installPossible) this.install();
+
+//       this.$router.push({ name: "home" });
+//     },
+//     install() {
+//       //Prompt for Installing
+//       let res = this.$confirm("Do you want install Sportest?", {
+//         title: "Install"
+//       });
+//       if (res) {
+//         try {
+//           deferredPrompt.prompt();
+//           // Wait for the user to respond to the prompt
+//           deferredPrompt.userChoice.then(choiceResult => {
+//             if (choiceResult.outcome === "accepted") {
+//               console.log("User accepted the A2HS prompt");
+//             } else {
+//               console.log("User dismissed the A2HS prompt");
+//             }
+//             deferredPrompt = null;
+//           });
+//         } catch (error) {
+//           console.log("Installieren fehlgeschlagen");
+//           console.log(error);
+//         }
+//       }
+//     },
+//   }
+// }; */
 </style> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
