3bd50a8aa8157ff73092758f9d42f5a615f9b4b8
Hackerberger <hechenberger.l00@htl-ottakring.ac.at>
2020-04-07 11:14:13 +0200

    router.js und views gesäubert

--- src/router.js
+++ src/router.js
@@ -1,49 +1,49 @@
-import Vue from "vue";
-import Router from "vue-router";
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import Login from './views/Login.vue';
+import Home from './views/Home.vue';
+import Home_L from './views/Home_lehrer.vue';
+import Test from './views/TestForm.vue';
+import Statistics from './views/Statistics.vue';
+import Statistics_L from './views/Statistics_lehrer.vue';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: "/",
-      name: "login",
-      component: () =>
-        import(/* webpackChunkName: "login" */ "./views/Login.vue")
+      path: '/',
+      name: 'login',
+      component: Login,
     },
     {
-      path: "/home",
-      name: "home",
-      component: () => import(/* webpackChunkName: "home" */ "./views/Home.vue")
+      path: '/home',
+      name: 'home',
+      component: Home,
     },
     {
-      path: "/home_l",
-      name: "home_l",
-      component: () =>
-        import(/* webpackChunkName: "home_l" */ "./views/Home_lehrer.vue")
+      path: '/home_l',
+      name: 'home_l',
+      component: Home_L,
     },
     {
-      path: "/test",
-      name: "test",
+      path: '/test',
+      name: 'test',
       props: true,
-      component: () =>
-        import(/* webpackChunkName: "test" */ "./components/TestForm.vue")
+      component: Test,
     },
     {
-      path: "/statistics",
-      name: "statistics",
+      path: '/statistics',
+      name: 'statistics',
       props: true,
-      component: () =>
-        import(/* webpackChunkName: "statistics" */ "./views/Statistics.vue")
+      component: Statistics,
     },
     {
-      path: "/statistics_l",
-      name: "statistics_l",
+      path: '/statistics_l',
+      name: 'statistics_l',
       props: true,
-      component: () =>
-        import(
-          /* webpackChunkName: "statistics_lehrer" */ "./views/Statistics_lehrer.vue"
-        )
-    }
-  ]
+      component: Statistics_L,
+    },
+  ],
 });
