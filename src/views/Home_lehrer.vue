78e02319cf5321eff055e31ba18b0a83a191ecda
Hackerberger <hechenberger.l00@htl-ottakring.ac.at>
2020-05-07 15:12:43 +0200

    Lehrer Home aufgeräumt

--- src/views/Home_lehrer.vue
+++ src/views/Home_lehrer.vue
@@ -1,18 +1,18 @@
 <template>
   <v-container grid-list-xs>
     <span class="white--text" id="welcome"
-      >Willkommen Professor Michael Aschberger!</span
+      >Willkommen Prof.
+      {{ globalData.googleUser.getBasicProfile().getName() }}</span
     >
     <span>
       <v-avatar size="36px">
-        <img src="./../../public/aschberger.jpg" />
+        <img :src="globalData.googleUser.getBasicProfile().getImageUrl()" />
       </v-avatar>
     </span>
     <br />
 
     <br />
     <span class="white--text">
-      <!-- {{ googleProfile.Name }} -->
       Hallo, sportest ermöglicht Ihren Schüler/innen die Messung und Bewertung
       ihrer motorischen Fähigkeiten. [Lehrer-Ansicht]
     </span>
@@ -25,59 +25,13 @@
 </template>
 
 <script>
-import Description from "../components/Descriptions.vue";
+import Description from '../components/Descriptions.vue';
 
 export default {
   components: { Description },
   data() {
-    return {
-      testData: [1, 2, 3],
-      zahl1: null,
-      zahl2: null,
-      googleProfile: {
-        profiledata: {},
-        ID: "",
-        Name: "",
-        GivenName: "",
-        FamilyName: "",
-        ImageUrl: "",
-        Email: ""
-      },
-      dialog: false
-    };
+    return {};
   },
-  created() {
-    //  this.$getItem("objectest")
-    //   .then(value => {
-    //     this.testData = value;
-    //   })
-    //   .catch(err => {
-    //     console.log("the rockets red glare has blinded me");
-    //   });
-    // this.googleProfile.profiledata = this.$parent.globalData.getBasicProfile();
-    // this.googleProfile.ID = this.googleProfile.profiledata.getId();
-    // this.googleProfile.Name = this.googleProfile.profiledata.getName();
-    // this.googleProfile.GivenName = this.googleProfile.profiledata.getGivenName();
-    // this.googleProfile.Email = this.googleProfile.profiledata.getEmail();
-    // this.googleProfile.ImageUrl = this.googleProfile.profiledata.getImageUrl();
-    //   console.log("ID: " + this.googleProfile.getId());
-    //   console.log("Full Name: " + this.googleProfile.getName());
-    //   console.log("Given Name: " + this.googleProfile.getGivenName());
-    //   console.log("Family Name: " + this.googleProfile.getFamilyName());
-    //   console.log("Image URL: " + this.googleProfile.getImageUrl());
-    //   console.log("Email: " + this.googleProfile.getEmail());
-  },
-  methods: {
-    add() {
-      this.$setItem("objectest", [this.zahl1, this.zahl2])
-        .then(value => {
-          console.log("woot! we saved " + value);
-        })
-        .catch(err => {
-          console.log("he's dead, jim!");
-        });
-    }
-  }
 };
 </script>
 
